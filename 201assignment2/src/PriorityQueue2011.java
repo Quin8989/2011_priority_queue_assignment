7a10f808e9a227552b50eeff09038e3904efb718
Q <Q@DESKTOP-60KPC6M>
2021-03-05 21:16:06 -0500

    Initial commit

---
+++ 201assignment2/src/PriorityQueue2011.java
@@ -0,0 +1,66 @@
+import java.util.AbstractQueue;
+import java.util.Iterator;
+
+public class PriorityQueue2011<E> extends AbstractQueue<E> {
+
+	@Override
+	public boolean offer(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean add(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E element() {
+		// TODO Auto-generated method stub
+		//ASASDADASDASDASD
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String toTree() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+}
