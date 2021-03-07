b884b2e80b64f9900e62e96a456e7b4b55e349e5
Q <Q@DESKTOP-60KPC6M>
2021-03-06 21:09:45 -0500

    argh!

--- 201assignment2/src/PQSort.java
+++ 201assignment2/src/PQSort.java
@@ -1,34 +1,31 @@
 
 import java.util.PriorityQueue;
 
-
 public class PQSort {
-    static <E extends Comparable<E>> void heapSort(E [] a) {
-        PriorityQueue<E> pQueue = new PriorityQueue<E>();
-        for(E i : a) {
-            System.out.println(i);
-            pQueue.offer(i);
-        }
-        System.out.println(" ");
-        for(int k = 0 ; k < a.length; k++) {
-            System.out.println(pQueue.poll());
-        }
-        System.out.println("");
-        
-    }
-    
+	static <E extends Comparable<? super E>> void heapSort(E[] a) {
+		PriorityQueue<E> pQueue = new PriorityQueue<E>();
+		for (E i : a) {
+			System.out.println(i);
+			pQueue.offer(i);
+		}
+		System.out.println(" ");
+		for (int k = 0; k < a.length; k++) {
+			System.out.println(pQueue.poll());
+		}
+		System.out.println("");
 
-    static <E extends Comparable<E>> void heapSort2011(E[] a) {
-        PriorityQueue2011<E> pQueue = new PriorityQueue2011<E>();
-        for(E i : a) {
-            System.out.println(i);
-            pQueue.offer(i);
-        }
-        System.out.println(" ");
-        for(int k = 0 ; k < a.length; k++) {
-            System.out.println(pQueue.poll());
-        }
-        
-        
-    }
+	}
+
+	static <E extends Comparable<? super E>> void heapSort2011(E[] a) {
+		PriorityQueue2011<E> pQueue = new PriorityQueue2011<E>();
+		for (E i : a) {
+			System.out.println(i);
+			pQueue.offer(i);
+		}
+		System.out.println(" ");
+		for (int k = 0; k < a.length; k++) {
+			System.out.println(pQueue.poll());
+		}
+	}
+
 }
